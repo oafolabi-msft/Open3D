d97df9103de9748f4109b641f09b5d3d681a3c63
Oladapo Afolabi <oafolabi@microsoft.com>
2021-11-06 20:12:45 -0700

    updating solution script

--- generate-sln.ps1
+++ generate-sln.ps1
@@ -2,15 +2,15 @@ $currentDirectory=Get-Location
 echo $currentDirectory
 
 # build for x64
-$x64_build_dir=$currentDirectory.Path + "\build_x64"
-$x64_install_dir=$currentDirectory.Path + "\install_x64" 
+$x64_build_dir=$currentDirectory.Path + "\build_X64"
+$x64_install_dir=$currentDirectory.Path + "\install_X64" 
 remove-item -path ($x64_build_dir) -Force -Recurse -ErrorAction SilentlyContinue 
 remove-item -path ($x64_install_dir) -Force -Recurse -ErrorAction SilentlyContinue  
 mkdir ($x64_build_dir)
 mkdir ($x64_install_dir)
 cd $x64_build_dir
 
-cmake -G "Visual Studio 16 2019" -A X64 -DCMAKE_INSTALL_PREFIX="../install_x64/" ..
+cmake -G "Visual Studio 16 2019" -A X64 -DCMAKE_INSTALL_PREFIX="../install_X64/" ..
 cmake --build . --config Release --target ALL_BUILD
 cmake --build . --config Release --target INSTALL
 
@@ -23,14 +23,14 @@ cd $currentDirectory.Path
 
 
 # build for arm64
-$arm64_build_dir=$currentDirectory.Path + "\build_arm64"
-$arm64_install_dir=$currentDirectory.Path + "\install_arm64" 
+$arm64_build_dir=$currentDirectory.Path + "\build_ARM64"
+$arm64_install_dir=$currentDirectory.Path + "\install_ARM64" 
 remove-item -path ($arm64_build_dir) -Force -Recurse -ErrorAction SilentlyContinue 
 remove-item -path ($arm64_install_dir) -Force -Recurse -ErrorAction SilentlyContinue  
 mkdir ($arm64_build_dir)
 mkdir ($arm64_install_dir) 
 cd $arm64_build_dir
-cmake -G "Visual Studio 16 2019" -A ARM64 -DCMAKE_INSTALL_PREFIX="../install_arm64/" ..
+cmake -G "Visual Studio 16 2019" -A ARM64 -DCMAKE_INSTALL_PREFIX="../install_ARM64/" ..
 cmake --build . --config Release --target ALL_BUILD
 cmake --build . --config Release --target INSTALL
 # copy dependencies over including system dlls
