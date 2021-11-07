468cca2ff6402f1a22ecbe72a532b41f8bcfb190
Oladapo Afolabi <oafolabi@microsoft.com>
2021-11-06 19:08:02 -0700

    updating gitignore and generate-sln

--- generate-sln.ps1
+++ generate-sln.ps1
@@ -10,7 +10,7 @@ mkdir ($x64_build_dir)
 mkdir ($x64_install_dir)
 cd $x64_build_dir
 
-cmake -G "Visual Studio 16 2019" -A X64 -DCMAKE_INSTALL_PREFIX=($x64_install_dir) ..
+cmake -G "Visual Studio 16 2019" -A X64 -DCMAKE_INSTALL_PREFIX="../install_x64/" ..
 cmake --build . --config Release --target ALL_BUILD
 cmake --build . --config Release --target INSTALL
 
@@ -23,20 +23,19 @@ cd $currentDirectory.Path
 
 
 # build for arm64
-remove-item -path ($currentDirectory.Path + "\build_arm64") -Force -Recurse -ErrorAction SilentlyContinue 
-remove-item -path ($currentDirectory.Path + "\install_arm64") -Force -Recurse -ErrorAction SilentlyContinue  
-mkdir ($currentDirectory.Path + "\build_arm64")
-mkdir ($currentDirectory.Path + "\install_arm64")
-$install_prefix=($currentDirectory.Path + "\install_arm64\") 
-cd ./build_arm64
-echo ($currentDirectory.Path + "\install_arm64\")
-# read-host “Press ENTER to continue...”
+$arm64_build_dir=$currentDirectory.Path + "\build_arm64"
+$arm64_install_dir=$currentDirectory.Path + "\install_arm64" 
+remove-item -path ($arm64_build_dir) -Force -Recurse -ErrorAction SilentlyContinue 
+remove-item -path ($arm64_install_dir) -Force -Recurse -ErrorAction SilentlyContinue  
+mkdir ($arm64_build_dir)
+mkdir ($arm64_install_dir) 
+cd $arm64_build_dir
 cmake -G "Visual Studio 16 2019" -A ARM64 -DCMAKE_INSTALL_PREFIX="../install_arm64/" ..
 cmake --build . --config Release --target ALL_BUILD
 cmake --build . --config Release --target INSTALL
 # copy dependencies over including system dlls
 # copy zlib files 
-cp ($currentDirectory.Path + "\build_arm64\zlib\bin\zlib.dll") ($currentDirectory.Path + "\install_arm64\bin\")
-cp ($currentDirectory.Path + "\build_arm64\zlib\lib\zlib.lib") ($currentDirectory.Path + "\install_arm64\lib\")
-cp ($currentDirectory.Path + "\build_arm64\zlib\lib\zlibstatic.lib") ($currentDirectory.Path + "\install_arm64\lib\")
+cp ($arm64_build_dir + "\zlib\bin\zlib.dll") ($arm64_install_dir + "\bin\")
+cp ($arm64_build_dir+ "\zlib\lib\zlib.lib") ($arm64_install_dir + "\lib\")
+cp ($arm64_build_dir + "\zlib\lib\zlibstatic.lib") ($arm64_install_dir + "\lib\")
 cd $currentDirectory.Path
